41d8860803b490e00357cfba95600414eedf3ab5
HirotoAoki <onkochisin419@gmail.com>
2020-07-13 21:06:41 +0900

    SNSアイコンリンクの追加

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,18 +5,40 @@ import React from "react";
 function Footer() {
   return (
     <footer className="text-center bg-gray-700">
-      <nav className="flex-none max-w-4xl p-2 mx-auto text-sm md:p-4">
-        <p className="text-white justify-center">
-          {` `}
-          <a
-            className="font-bold no-underline"
-            href="https://hiroto-aoki.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            © 2013 hiroto-aoki.com
+      <nav className="max-w-4xl p-2 mx-auto text-sm md:p-2 text-white justify-center">
+        <div className="flex max-w-4xl p-2 mx-auto text-sm md:p-2 text-white justify-center">
+        <a href="https://github.com/HirotoAoki" target="_blank" rel="noreferrer noopener" aria-label="github" className="text-grey hover:text-grey-darker mx-3">          
+            <svg viewBox="0 0 24 24" className="fill-current w-8 h-8 hover:text-grey-darker">
+              <path d="m12 .5c-6.63 0-12 5.28-12 11.792 0 5.211 3.438 9.63 8.205 11.188.6.111.82-.254.82-.567 0-.28-.01-1.022-.015-2.005-3.338.711-4.042-1.582-4.042-1.582-.546-1.361-1.335-1.725-1.335-1.725-1.087-.731.084-.716.084-.716 1.205.082 1.838 1.215 1.838 1.215 1.07 1.803 2.809 1.282 3.495.981.108-.763.417-1.282.76-1.577-2.665-.295-5.466-1.309-5.466-5.827 0-1.287.465-2.339 1.235-3.164-.135-.298-.54-1.497.105-3.121 0 0 1.005-.316 3.3 1.209.96-.262 1.98-.392 3-.398 1.02.006 2.04.136 3 .398 2.28-1.525 3.285-1.209 3.285-1.209.645 1.624.24 2.823.12 3.121.765.825 1.23 1.877 1.23 3.164 0 4.53-2.805 5.527-5.475 5.817.42.354.81 1.077.81 2.182 0 1.578-.015 2.846-.015 3.229 0 .309.21.678.825.56 4.801-1.548 8.236-5.97 8.236-11.173 0-6.512-5.373-11.792-12-11.792z"/>
+            </svg>
           </a>
-        </p>
+          <a href="https://www.facebook.com/HirokazuAoki" target="_blank" rel="noreferrer noopener" aria-label="facebook" className="text-grey hover:text-grey-darker mx-3">          
+            <svg viewBox="0 0 24 24" className="fill-current w-8 h-8">
+              <path d="M19,4V7H17A1,1 0 0,0 16,8V10H19V13H16V20H13V13H11V10H13V7.5C13,5.56 14.57,4 16.5,4M20,2H4A2,2 0 0,0 2,4V20A2,2 0 0,0 4,22H20A2,2 0 0,0 22,20V4C22,2.89 21.1,2 20,2Z" />
+            </svg>
+          </a>
+          <a href="https://www.instagram.com/hiroto_aoki/" target="_blank" rel="noreferrer noopener" aria-label="instagram" className="text-grey hover:text-grey-darker mx-3">          
+            <svg viewBox="0 0 24 24" className="fill-current w-8 h-8">
+              <path d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z" />
+            </svg>
+          </a>
+          <a href="https://twitter.com/Hiroto_Aoki" target="_blank" rel="noreferrer noopener" aria-label="twitter" className="text-grey hover:text-grey-darker mx-3">          
+            <svg viewBox="0 0 24 24" className="fill-current w-8 h-8">
+              <path d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z" />
+            </svg>
+          </a>
+
+        </div>
+
+        <a
+          className="flex-none font-bold no-underline"
+          href="https://hiroto-aoki.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          © 2013 hiroto-aoki.com
+        </a>
+
       </nav>
     </footer>
   );
