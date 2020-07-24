b94f5dbb13b0db27acc198699be6e1c1e8403220
HirotoAoki <onkochisin419@gmail.com>
2020-07-24 21:11:26 +0900

    キャリアページに実績を追加

--- src/pages/career.js
+++ src/pages/career.js
@@ -7,7 +7,8 @@ import workImage2 from "../images/senku.png";
 import workImage3 from "../images/beachtown.png";
 import portfolioImage1 from "../images/hiroto-aoki.png";
 import portfolioImage2 from "../images/beach-hayama.png";
-import portfolioImage3 from "../images/view.jpg";
+import portfolioImage3 from "../images/tep.png";
+import portfolioImage4 from "../images/view.jpg";
 import skillImage1 from "../images/frontend.svg";
 import skillImage2 from "../images/backend.svg";
 import skillImage3 from "../images/tools.svg";
@@ -39,13 +40,13 @@ function CareerPage() {
           在籍した会社での職務内容です
         </p>
         <div className="my-4 flex flex-col md:flex-row rounded shadow-lg">
-          <img className="px-4 py-4 md:w-1/3 w-full h-full" src={workImage1} alt="Fujitsu" />
+          <img className="px-4 py-4 md:w-1/2 w-full h-full" src={workImage1} alt="Fujitsu" />
           <div className="px-6 py-4">
             <p className="font-bold text-lg mb-2">富士通株式会社</p>
             <p className="italic text-md mb-2">2007年4月〜2015年2月</p>
             <p className="my-2 text-gray-700 text-base">
-              顧客管理システムのプロジェクトに参画しシステム設計やコーティングを担当し、システム開発について多くのことを学びました。<br />
-              在籍中に個人活動としてビジネスモデルを考案し特許出願しました。
+              顧客管理システムのプロジェクトに参画しシステム設計やコーティングを担当しました。メンテナンスを容易にするために独自フレームワークを実装するなど、柔軟なシステム構築の概念を学びました。<br />
+              在籍中に個人活動としてビジネスモデルを考案し特許出願しています。
             </p>
             <div className="py-4">
               <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">設計</span>
@@ -55,7 +56,7 @@ function CareerPage() {
           </div>
         </div>
         <div className="my-4 flex flex-col md:flex-row rounded shadow-lg">
-          <img className="px-4 py-4 md:w-1/3 w-full h-full" src={workImage2} alt="Fujitsu" />
+          <img className="px-4 py-4 md:w-1/2 w-full h-full" src={workImage2} alt="Fujitsu" />
           <div className="px-6 py-4">
             <p className="font-bold text-lg mb-2">株式会社 先駆</p>
             <p className="italic text-md mb-2">2015年2月〜2015年7月</p>
@@ -71,13 +72,13 @@ function CareerPage() {
           </div>
         </div>
         <div className="my-4 flex flex-col md:flex-row rounded shadow-lg">
-          <img className="px-4 py-4 md:w-1/3 w-full h-full" src={workImage3} alt="BEACHTOWN" />
+          <img className="px-4 py-4 md:w-1/2 w-full h-full" src={workImage3} alt="BEACHTOWN" />
           <div className="px-6 py-4">
             <p className="font-bold text-lg mb-2">株式会社 BEACHTOWN</p>
             <p className="italic text-md mb-2">2015年7月〜2020年4月</p>
             <p className="my-2 text-gray-700 text-base">
-              フィットネスプログラムの指導、および施設開設のプロデュース、社内SEを兼務しWeb予約サイトの構築を担当しました。<br />
-              プロジェクトリーダーとしてマネージメント、課題解決力が養われる経験となりました。
+              フィットネスプログラムの指導、および施設開設のディレクション、社内SEを兼務しWeb予約サイトの構築を担当しました。<br />
+              マネージメント、課題解決力が養われる経験となりました。
             </p>
             <div className="py-4">
               <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">インストラクター</span>
@@ -139,12 +140,13 @@ function CareerPage() {
           <div className="lg:w-1/2 md:w-1/2 my-4 flex flex-col md:flex-row rounded shadow-lg">
             <img className="px-4 py-4 md:w-1/3 w-full h-full" src={skillImage4} alt="フィットネス" />
             <div className="px-6 py-4">
-              <p className="text-center font-bold text-lg my-2">フィットネス指導</p>
+              <p className="text-center font-bold text-lg my-2">フィットネス関連</p>
               <p className="text-gray-700 text-base">
               <ul>
-                <li>ランニング </li>
-                <li>ノルディックウォーキング</li>
-                <li>ピラティス</li>
+                <li>ランニング指導 </li>
+                <li>ノルディックウォーキング指導</li>
+                <li>体幹トレーニング指導</li>
+                <li>フィットネス施設開設ディレクション</li>
               </ul>
               </p>
             </div>
@@ -159,9 +161,9 @@ function CareerPage() {
         制作した成果物です
         </p>
         <div className="my-4 flex flex-col md:flex-row rounded shadow-lg">
-          <img className="px-4 py-4 md:w-1/3 w-full h-full" src={portfolioImage1} alt="dev.hiroto-aoki" />
+          <img className="px-4 py-4 md:w-1/2 w-full h-full" src={portfolioImage1} alt="dev.hiroto-aoki" />
           <div className="px-6 py-4">
-            <p className="font-bold text-lg mb-2">ポートフォリオサイト</p>
+            <p className="font-bold text-lg mb-2">個人ポートフォリオサイト</p>
             <p className="my-2 text-gray-700 text-base">
               本サイトです。経歴や技術的スキルについてまとめています。
             </p>
@@ -183,11 +185,11 @@ function CareerPage() {
           </div>
         </div>
         <div className="my-4 flex flex-col md:flex-row rounded shadow-lg">
-          <img className="px-4 py-4 md:w-1/3 w-full h-full" src={portfolioImage2} alt="beach-hayama" />
+          <img className="px-4 py-4 md:w-1/2 w-full h-full" src={portfolioImage2} alt="beach-hayama" />
           <div className="px-6 py-4">
             <p className="font-bold text-lg mb-2">BEACH 葉山 Web予約サイト</p>
             <p className="my-2 text-gray-700 text-base">
-              フィットネスクラブの会員向けレッスン予約のWebサイトです。
+              フィットネスクラブの会員向けレッスン予約のWebサイトです。<br />システム開発を担当しています。
             </p>
             <div className="py-4 text-justify">
               <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">PHP</span>
@@ -196,18 +198,32 @@ function CareerPage() {
             </div>
             <button className="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-1 px-3 mr-2 border border-green-500 hover:border-transparent rounded">
               <a href="https://outdoorfitness-club.com/schedule/hayama/" target="_blank" rel="noopener noreferrer">
-                DEMO
-              </a>
-            </button>
-            <button className="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-1 px-3 mr-2 border border-green-500 hover:border-transparent rounded">
-              <a href="https://github.com/HirotoAoki/outdoorfitness-club.com" target="_blank" rel="noopener noreferrer">
-                Github
+                オフィシャルサイト
               </a>
             </button>
           </div>
         </div>
         <div className="my-4 flex flex-col md:flex-row rounded shadow-lg">
-          <img className="px-4 py-4 md:w-1/3 w-full h-full" src={portfolioImage3} alt="" />
+          <img className="px-4 py-4 md:w-1/2 w-full h-full" src={portfolioImage3} alt="beach-hayama" />
+          <div className="px-6 py-4">
+            <p className="font-bold text-lg mb-2">アウトドアフィットネス Tokyo East Park</p>
+            <p className="my-2 text-gray-700 text-base">
+              東京都都立公園のフィットネスプログラム導入案件です。<br />ディレクション、Webサイトデザイン、マーケティングを担当しています。
+            </p>
+            <div className="py-4 text-justify">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">ディレクション</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Illustrator</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">PHP</span>
+            </div>
+            <button className="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-1 px-3 mr-2 border border-green-500 hover:border-transparent rounded">
+              <a href="https://tokyo-eastpark.com/outdoor/" target="_blank" rel="noopener noreferrer">
+                オフィシャルサイト
+              </a>
+            </button>
+          </div>
+        </div>
+        <div className="my-4 flex flex-col md:flex-row rounded shadow-lg">
+          <img className="px-4 py-4 md:w-1/2 w-full h-full" src={portfolioImage4} alt="" />
           <div className="px-6 py-4">
             <p className="font-bold text-lg mb-2">Laravel オリジナルシステム</p>
             <p className="my-2 text-gray-700 text-base">
